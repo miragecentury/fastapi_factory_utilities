7d04a7be8dfb413337acd23c3361757e53eb44ef
miragecentury <victorien.vanroye@gmail.com>
2025-01-15 08:24:00 +0100

    refactor: exception for GenericConfigBuilder

--- src/fastapi_factory_utilities/core/app/exceptions.py
+++ src/fastapi_factory_utilities/core/app/exceptions.py
@@ -1,5 +1,7 @@
 """Provides the exceptions for the application factory."""
 
+from ..exceptions import FastAPIFactoryUtilitiesError
+
 
 class BaseApplicationException(BaseException):
     """Base application exception."""
@@ -7,7 +9,12 @@ class BaseApplicationException(BaseException):
     pass
 
 
-class ApplicationConfigFactoryException(BaseApplicationException):
+class ConfigBuilderError(FastAPIFactoryUtilitiesError):
     """Application configuration factory exception."""
 
-    pass
+    def __init__(self, message: str, config_class: type, package: str, filename: str) -> None:
+        """Instantiate the exception."""
+        super().__init__(
+            message=f"Unable to build the configuration for the package {package} and "
+            + f"the file {filename} with the class {config_class} - {message}"
+        )
