2759816a30a0ed430adedc0e3dd5e48de3dcc50c
miragecentury <victorien.vanroye@gmail.com>
2025-01-15 17:06:45 +0100

    refactor: BaseError and add tracing in it

--- src/fastapi_factory_utilities/core/exceptions.py
+++ src/fastapi_factory_utilities/core/exceptions.py
@@ -1,7 +1,9 @@
 """FastAPI Factory Utilities exceptions."""
 
 import logging
+from typing import Any
 
+from opentelemetry.trace import Span, get_current_span
 from structlog.stdlib import BoundLogger, get_logger
 
 _logger: BoundLogger = get_logger()
@@ -12,12 +14,30 @@ class FastAPIFactoryUtilitiesError(Exception):
 
     def __init__(
         self,
-        *args: object,
+        *args: tuple[Any],
         message: str | None = None,
         level: int = logging.ERROR,
+        **kwargs: dict[str, Any],
     ) -> None:
-        """Instanciate the exception."""
+        """Instanciate the exception.
+
+        Args:
+            *args (Tuple[Any]): The arguments.
+            message (str | None): The message.
+            level (int): The logging level.
+            **kwargs (dict[str, Any]): The keyword arguments
+        """
+        # Log the Exception
         if message:
             _logger.log(level=level, event=message)
-            args = (message, *args)
-        super().__init__(*args)
+            self.message = message
+            self.level = level
+            args = (message, *args)  # type: ignore
+        # Propagate the exception
+        span: Span = get_current_span()
+        # If not otel is setup, INVALID_SPAN is retrive from get_current_span
+        # and it will respond False to the is_recording method
+        if span.is_recording():
+            span.record_exception(self)
+        # Call the parent class
+        super().__init__(*args, **kwargs)
