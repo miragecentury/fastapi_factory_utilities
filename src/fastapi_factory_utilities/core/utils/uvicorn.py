002e9a3690528bc111b7eeb3c1cb435c01ca9763
miragecentury <victorien.vanroye@gmail.com>
2025-02-22 01:44:24 +0100

    feat(fastapi): manage ssl materials

--- src/fastapi_factory_utilities/core/utils/uvicorn.py
+++ src/fastapi_factory_utilities/core/utils/uvicorn.py
@@ -1,5 +1,8 @@
 """Provides utilities for the application."""
 
+import os
+from typing import Any
+
 import uvicorn
 import uvicorn.server
 
@@ -20,6 +23,29 @@ class UvicornUtils:
             None
         """
         self._app: ApplicationAbstractProtocol = app
+        self._ssl_keyfile: str | os.PathLike[str] | None = None
+        self._ssl_certfile: str | os.PathLike[str] | None = None
+        self._ssl_keyfile_password: str | None = None
+
+    def add_ssl_certificates(
+        self,
+        ssl_keyfile: str | os.PathLike[str] | None = None,
+        ssl_certfile: str | os.PathLike[str] | None = None,
+        ssl_keyfile_password: str | None = None,
+    ) -> None:
+        """Add SSL certificates to the application.
+
+        Args:
+            ssl_keyfile (str | os.PathLike[str] | None): The SSL key file.
+            ssl_certfile (str | os.PathLike[str] | None): The SSL certificate file.
+            ssl_keyfile_password (str | None): The SSL key file password.
+
+        Returns:
+            None
+        """
+        self._ssl_keyfile = ssl_keyfile
+        self._ssl_certfile = ssl_certfile
+        self._ssl_keyfile_password = ssl_keyfile_password
 
     def build_uvicorn_config(self) -> uvicorn.Config:
         """Build the Uvicorn configuration.
@@ -27,12 +53,22 @@ class UvicornUtils:
         Returns:
             uvicorn.Config: The Uvicorn configuration.
         """
+        kwargs: dict[str, Any] = {}
+
+        if self._ssl_keyfile:
+            kwargs["ssl_keyfile"] = self._ssl_keyfile
+        if self._ssl_certfile:
+            kwargs["ssl_certfile"] = self._ssl_certfile
+        if self._ssl_keyfile_password:
+            kwargs["ssl_keyfile_password"] = self._ssl_keyfile_password
+
         config = uvicorn.Config(
             app=self._app.get_asgi_app(),
             host=self._app.get_config().server.host,
             port=self._app.get_config().server.port,
             reload=self._app.get_config().development.reload,
             workers=self._app.get_config().server.workers,
+            **kwargs,
         )
         clean_uvicorn_logger()
         return config
